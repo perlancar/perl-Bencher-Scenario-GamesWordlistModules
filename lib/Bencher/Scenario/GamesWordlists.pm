14c6baeb47f8b4705fb6a35ef761517dcffe072e
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-07 21:18:52 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/GamesWordlists.pm
@@ -0,0 +1,25 @@
+package Bencher::Scenario::GamesWordlists;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark startup overhead of Games::Word::Wordlist::* modules',
+    module_startup => 1,
+    participants => [
+        {module=>'Games::Word::Wordlist::Country'},
+        {module=>'Games::Word::Wordlist::CountrySingleWord'},
+        {module=>'Games::Word::Wordlist::Enable'},
+        {module=>'Games::Word::Wordlist::KBBI'},
+        {module=>'Games::Word::Wordlist::SGB'},
+        {module=>'Games::Word::Phraselist::Proverb::KBBI'},
+        {module=>'Games::Word::Phraselist::Proverb::TWW'},
+    ],
+};
+
+1;
+# ABSTRACT:
